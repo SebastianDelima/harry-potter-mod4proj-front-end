5a47988ae15ba262f96fb85d95ca4281f98ac85c
github username <github email address>
2020-01-17 17:07:28 -0500

    working on hat component

--- harry-project-client/src/components/Hat.js
+++ harry-project-client/src/components/Hat.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Hat extends Component{
-    // let objectConfig = {
-    //     method: 'POST',
-    //     headers: {
-    //        'Content-Type':'application/json'
-    //     }, 
-    //     body: JSON.stringify({
-    //       name: this.state.logInUser
-    //     })
-    
+
+    constructor(){
+        super()
+        this.state = {
+            house: null
+        }
+    }
+
+    setHouse = () => {
+        this.setState({
+            house: ''
+        })
+    }
+
+    createUser(){
+        let objectConfig = {
+        method: 'POST',
+        headers: {
+           'Content-Type':'application/json'
+        }, 
+        body: JSON.stringify({
+          name: this.props.userName,
+          house: ''
+        })
+    }
+
+}
+   
     sortHouse = () => {
 
         let houses = ['Gryffindor', 'Hufflepuff', 'Slytherin', 'Ravenclaw']
@@ -24,6 +44,7 @@ export default class Hat extends Component{
             <div>
                <img alt='sorting hat' src='https://media1.tenor.com/images/426045f4dc47e5bfaaa2b095ed179895/tenor.gif?itemid=13986854'></img>
                <h1>Your destiny is: {this.sortHouse()} </h1>
+               <Link>Continue your magical journey!</Link>
             </div>
         )
         
