2a90a0f7df168843b265a0b03f1ba822f541cc59
github username <github email address>
2020-01-22 11:19:55 -0500

    working on button

--- harry-project-client/src/components/Main.js
+++ harry-project-client/src/components/Main.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import {  NavLink } from 'react-router-dom';
 
 class Main extends React.Component{
   render(){
       return(
+        <Fragment>
       <div>
-       <NavLink to='/characters' exact>Characters</NavLink>
-       <p></p>
-       <NavLink to='/spells' exact>Spells</NavLink>
-       <p></p>
-       <NavLink to='/favorites' exact>Favorites</NavLink>
-      </div>)
+
+        <div className="sidenav">
+          <a href="#about">About</a>
+          <p></p>
+          
+          <p></p>
+          
+          <p></p>
+          <NavLink to='/favorites' exact>Favorites</NavLink>
+        </div>
+        
+        <div className="backgroundMain">
+          <button className="btn btn-dark characterMain"><NavLink to='/characters' exact>Characters</NavLink></button>
+          <button className="btn btn-dark spellMain"><NavLink to='/spells' exact>Spells</NavLink></button>
+        </div>
+       
+      </div>
+      </Fragment>
+      )
+      
   }
 }
 
