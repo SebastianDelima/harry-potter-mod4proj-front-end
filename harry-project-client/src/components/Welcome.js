26a058da27a32f1fcdca9cc8cca972ad38f3e100
github username <github email address>
2020-01-22 11:19:55 -0500

    working on button

--- harry-project-client/src/components/Welcome.js
+++ harry-project-client/src/components/Welcome.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 export default class Welcome extends Component{
-  
+
     render() {
-        console.log("hello")
         return (
-            <div>
-            <h1>wefojwoifjaiofjaiofje</h1>
+            <div className="backgroundWelcome">
+                <h1 className="welcomePhase">Harry potter's Web Page</h1>
+                <button className="btn btn-secondary welcome"><h1>Sign Up!</h1></button>
+                <button className="btn btn-secondary welcome"><h1>Log in!</h1></button>
             </div>
         );
     }
